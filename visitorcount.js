ac2093f2b38f1d26c5a41b8c026ebb891d1edd2a
Tyler <universablu@gmail.com>
2021-10-26 17:57:06 -0500

    first attempt at getting counter displaying

--- visitorcount.js
+++ visitorcount.js
@@ -1,5 +1,11 @@
 const countEl = document.getElementById('VisitorCount');
 
+updateVisitorCount();
+
 function updateVisitorCount(){
-    fetch('')
+    fetch('https://api.countapi.xyz/update/d2coogfoy12yjk.cloudfront.net/VisitorCount/?amount=1')
+    .then(res => res.json())
+    .then(res => {
+        countEl.innerHTML = res.value;
+    });
 }
